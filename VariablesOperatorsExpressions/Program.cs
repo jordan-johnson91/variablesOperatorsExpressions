3143b32d54fa7fb47c27ba0b8b9a101ab0385273
Jordan Johnson <jordan@wecancodeit.org>
2017-08-16 18:36:02 -0400

    Worked on practice problems

--- VariablesOperatorsExpressions/Program.cs
+++ VariablesOperatorsExpressions/Program.cs
@@ -10,43 +10,87 @@ namespace VariablesOperatorsExpressions
     {
         static void Main(string[] args)
         {
-            string name = "Jordan";
-            //Console.WriteLine(name);
+            //string name = "Jordan";
+            ////Console.WriteLine(name);
 
-            name = "Dave";
-            //Console.WriteLine(name);
-            //Case matters when naming variables 
-            string Name = "Jordan";
+            //name = "Dave";
+            ////Console.WriteLine(name);
+            ////Case matters when naming variables 
+            //string Name = "Jordan";
 
-            string birthMonth = "December";
+            //string birthMonth = "December";
 
-            //Console.WriteLine(birthMonth);
-            //integers
-            int birthMonthNumber = 12;
-            int age = 65;
-            int population = 100000;
-            //Floating type variables 
-            float number = 2.164403304897294383961032f;
-            double someNumber = 2.164403304897294383961032d;
-            decimal partialNumber = 2.164403304897294383961032m;
+            ////Console.WriteLine(birthMonth);
+            ////integers
+            //int birthMonthNumber = 12;
+            //int age = 65;
+            //int population = 100000;
+            ////Floating type variables 
+            //float number = 2.164403304897294383961032f;
+            //double someNumber = 2.164403304897294383961032d;
+            //decimal partialNumber = 2.164403304897294383961032m;
 
-            //Console.WriteLine(number);
-            //Console.WriteLine(someNumber);
-            //Console.WriteLine(partialNumber);
+            ////Console.WriteLine(number);
+            ////Console.WriteLine(someNumber);
+            ////Console.WriteLine(partialNumber);
 
 
 
-            //Boolean 
-            bool isPresent = true;
-            bool seatTaken = false;
+            ////Boolean 
+            //bool isPresent = true;
+            //bool seatTaken = false;
 
-            //Character
-            char lastLetter = 'j';
+            ////Character
+            //char lastLetter = 'j';
 
-            int jessicaAge = 23;
-            int samAge = 47;
-            Console.WriteLine(jessicaAge > samAge);
-            Console.WriteLine(jessicaAge + samAge);
+            //int jessicaAge = 23;
+            //int samAge = 47;
+            //int total = samAge + jessicaAge;
+            //Console.WriteLine(jessicaAge > samAge);
+            //Console.WriteLine(jessicaAge + samAge);
+
+
+            //Practice problems turning english into c#
+
+            int friends = 4;
+            int billEven = 13;
+            int total = billEven * friends;
+           // Console.WriteLine(total);
+
+            int mikeBill = 13;
+            int frankBill = 13;
+            int daveBill = 13;
+            int jimBill = 13;
+
+            //Console.WriteLine(mikeBill + frankBill + daveBill + jimBill);
+
+            int sugar = 7;
+            int sugarUsed = 2;
+            //Console.WriteLine(sugar - sugarUsed);
+
+            int one = 8;
+            int moneyTotal = 40;
+            //Console.WriteLine(5 / 3);
+            //Console.WriteLine(moneyTotal / one);
+
+            int allMoney = 41;
+            int weekendMoney = 29;
+            // Console.WriteLine(allMoney - weekendMoney);
+
+            int juliaMiles = 47;
+            int previousmiles = 30;
+            int anotherGirlsMiles = juliaMiles - previousmiles;
+            Console.WriteLine(anotherGirlsMiles);
+
+            int totalMoney = 12;
+            int envelopeCost = 3;
+            int envelopes = totalMoney / envelopeCost;
+            Console.WriteLine(envelopes);
+
+            float saladPrice = 5.12f;
+            float endOfBalance = 27.10f;
+            Console.WriteLine(saladPrice + endOfBalance);
+
 
 
         }
