d5236e62d1c816d8f0cd37fff12cabeaea038f9b
Jordan Johnson <jordan@wecancodeit.org>
2017-08-15 20:50:36 -0400

    Created more variable types

--- VariablesOperatorsExpressions/Program.cs
+++ VariablesOperatorsExpressions/Program.cs
@@ -10,6 +10,45 @@ namespace VariablesOperatorsExpressions
     {
         static void Main(string[] args)
         {
+            string name = "Jordan";
+            //Console.WriteLine(name);
+
+            name = "Dave";
+            //Console.WriteLine(name);
+            //Case matters when naming variables 
+            string Name = "Jordan";
+
+            string birthMonth = "December";
+
+            //Console.WriteLine(birthMonth);
+            //integers
+            int birthMonthNumber = 12;
+            int age = 65;
+            int population = 100000;
+            //Floating type variables 
+            float number = 2.164403304897294383961032f;
+            double someNumber = 2.164403304897294383961032d;
+            decimal partialNumber = 2.164403304897294383961032m;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(someNumber);
+            //Console.WriteLine(partialNumber);
+
+
+
+            //Boolean 
+            bool isPresent = true;
+            bool seatTaken = false;
+
+            //Character
+            char lastLetter = 'j';
+
+            int jessicaAge = 23;
+            int samAge = 47;
+            Console.WriteLine(jessicaAge > samAge);
+            Console.WriteLine(jessicaAge + samAge);
+
+
         }
     }
 }
