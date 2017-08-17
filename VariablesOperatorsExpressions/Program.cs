079d086a8b2fa1f0f84b87a4460392b42fc8de2a
Jordan Johnson <jordan@wecancodeit.org>
2017-08-16 20:35:44 -0400

    finished practiced problems

--- VariablesOperatorsExpressions/Program.cs
+++ VariablesOperatorsExpressions/Program.cs
@@ -64,34 +64,54 @@ namespace VariablesOperatorsExpressions
 
             //Console.WriteLine(mikeBill + frankBill + daveBill + jimBill);
 
-            int sugar = 7;
-            int sugarUsed = 2;
-            //Console.WriteLine(sugar - sugarUsed);
+            //int sugar = 7;
+            //int sugarUsed = 2;
+            ////Console.WriteLine(sugar - sugarUsed);
 
-            int one = 8;
-            int moneyTotal = 40;
-            //Console.WriteLine(5 / 3);
-            //Console.WriteLine(moneyTotal / one);
+            //int one = 8;
+            //int moneyTotal = 40;
+            ////Console.WriteLine(5 / 3);
+            ////Console.WriteLine(moneyTotal / one);
 
-            int allMoney = 41;
-            int weekendMoney = 29;
-            // Console.WriteLine(allMoney - weekendMoney);
-
-            int juliaMiles = 47;
-            int previousmiles = 30;
-            int anotherGirlsMiles = juliaMiles - previousmiles;
-            Console.WriteLine(anotherGirlsMiles);
-
-            int totalMoney = 12;
-            int envelopeCost = 3;
-            int envelopes = totalMoney / envelopeCost;
-            Console.WriteLine(envelopes);
-
-            float saladPrice = 5.12f;
-            float endOfBalance = 27.10f;
-            Console.WriteLine(saladPrice + endOfBalance);
+            //int allMoney = 41;
+            //int weekendMoney = 29;
+            //// Console.WriteLine(allMoney - weekendMoney);
 
 
+            //int totalNumber = 72;
+            //int middleNumber = totalNumber / 3;
+            //int maxNumber = middleNumber + 1;
+            //int smallNumber = middleNumber - 1;
+            ////Console.WriteLine(smallNumber);
+
+            //int students = 331;
+            //int buses = 6;
+            //int driven = 7;
+            //int studentPerBus = (students - driven) / buses;
+            ////Console.WriteLine(studentPerBus);
+
+            //int sum = 48;
+            //int numofNumbers = 3;
+            //int average = sum / numofNumbers;
+            //int smallestNumber = average - 2;
+            //Console.WriteLine("This smallest number is " + smallestNumber);
+
+
+
+            //int total1 = 48;
+            //int number1 = (total1 - 6) / 3;
+            //Console.WriteLine(number1);
+
+            //int sumOfEvenNumbers = 48;
+            //int evenOne = sumOfEvenNumbers / 3;
+            //int evenTwo = evenOne - 2;
+            //int evenThree = evenOne + 2;
+            //Console.WriteLine(evenTwo);
+
+            int boughtBoxes = 7;
+            int boxesLeft = 22;
+            int boxesBeforeFire = boxesLeft * 2;
+            Console.WriteLine(boxesBeforeFire);
 
         }
     }
